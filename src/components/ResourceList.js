ac710939fd7fab4b8172ac6c925884e1d3a24009
Phonekham <phonekham.dev@gmail.com>
2019-06-25 11:48:40 +0700

    7. Building the ResourceList

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import axios from "axios";
 
 class ResourceList extends React.Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+    this.setState({ resources: response.data });
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.props.resource}</h1>
+        <div>{this.state.resources.length}</div>
       </div>
     );
   }
